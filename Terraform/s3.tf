e87f16068e64e775da58a5509e9b8f411884aa40
Anthony Cheung <acheung@Anthonys-MacBook-Air.local>
2023-01-11 11:51:13 +0000

    Added directories

---
+++ Terraform/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "crcfrontendac2023" {
+  bucket = "crcfrontendac2023"
+  acl    = "private"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
